12672878ca0900de2e5c7bbf789efb98529d0bd7
shubhamkarnalkar <shubhamkar.nalkar@gmail.com>
2024-05-20 11:20:55 +0000

    step 21: Not stable

--- webapp/controller/HomeScreen.controller.ts
+++ webapp/controller/HomeScreen.controller.ts
@@ -1,23 +1,34 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import Dialog from "sap/m/Dialog";
 
 /**
- * @namespace handheldd.controller.HomeScreen
+ * @namespace handheldd.controller
  */
 export default class HomeScreen extends Controller {
+  private dialog: Dialog;
   /*eslint-disable @typescript-eslint/no-empty-function*/
   public onInit(): void {
-    const data = {
-      recipient: {
-        name: ""
-      }
-    };
-    const dataModel = new JSONModel(data);
-    this.getView()?.setModel(dataModel);
+    const viewModel = new JSONModel({
+      currency: "EUR"
+    });
+    this.getView()?.setModel(viewModel, "view");
   }
 
-  public goToTransactions(): void {
+  public onTransactionsButtonPress(): void {
     MessageToast.show("Hey there");
+  }
+  async onOpenDialog(): Promise<void> {
+    this.dialog ??= await <Promise<Dialog>>this.loadFragment({
+      name: "handheldd.view.HelloDialog"
+    });
+    this.dialog.open();
+  }
+
+  onCloseDialog(): void {
+    // note: We don't need to chain to the pDialog promise, since this event-handler
+    // is only called from within the loaded dialog itself.
+    (<Dialog>this.byId("helloDialog"))?.close();
   }
 }
---
+++ webapp/controller/Transactions.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+import Dialog from "sap/m/Dialog";
+/**
+* @namespace handheldd.controller
+*/
+export default class Transactions extends Controller {
+    private dialog: Dialog;
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+    }
+    
+}
